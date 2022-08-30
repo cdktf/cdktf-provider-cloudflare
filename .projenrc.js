554453e5595a0f0b5dda761fe1aa637187f4f7ae
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-08-30 20:45:01 +0000

    chore(deps): upgrade provider project

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,6 @@
 const { CdktfProviderProject } = require("@cdktf/provider-project");
 const project = new CdktfProviderProject({
+  useCustomGithubRunner: false,
   terraformProvider: "cloudflare/cloudflare@~> 3.14",
   cdktfVersion: "^0.12.2",
   constructsVersion: "^10.0.0",
