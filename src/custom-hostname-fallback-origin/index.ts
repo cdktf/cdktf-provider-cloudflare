c834bd55d234e2a25e3c107229dfbf7877309dc2
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-06-01 10:47:01 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/custom-hostname-fallback-origin/index.ts
+++ src/custom-hostname-fallback-origin/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.7.1/docs/resources/custom_hostname_fallback_origin
 // generated from terraform resource schema
 
