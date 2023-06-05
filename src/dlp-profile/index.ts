5e2ae95cfab2d672b436a2122ffa2dbea0b072d7
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-06-05 14:43:46 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/dlp-profile/index.ts
+++ src/dlp-profile/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.7.1/docs/resources/dlp_profile
 // generated from terraform resource schema
 
