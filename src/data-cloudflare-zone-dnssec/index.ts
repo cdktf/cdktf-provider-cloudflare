91aac3fe7e0111d4c03dc7710b6028e7027cb810
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-01-15 11:18:40 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@5796aa9

--- src/data-cloudflare-zone-dnssec/index.ts
+++ src/data-cloudflare-zone-dnssec/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.22.0/docs/data-sources/zone_dnssec
 // generated from terraform resource schema
 
