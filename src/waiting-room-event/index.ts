f183a3629e81dd0e11013d2a240736bd9d641996
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-02-05 10:19:40 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/waiting-room-event/index.ts
+++ src/waiting-room-event/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.23.0/docs/resources/waiting_room_event
 // generated from terraform resource schema
 
