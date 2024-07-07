ed4b72dd9154d781aa185c7287245a06b257c57b
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-07 05:48:00 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@f32696c

--- src/custom-pages/index.ts
+++ src/custom-pages/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.36.0/docs/resources/custom_pages
 // generated from terraform resource schema
 
