c288c677dfe7453c44896fa667cb777aea969a7d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-15 07:58:47 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@a3a9bc4

--- src/api-shield-schema/index.ts
+++ src/api-shield-schema/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.37.0/docs/resources/api_shield_schema
 // generated from terraform resource schema
 
--- src/device-policy-certificates/index.ts
+++ src/device-policy-certificates/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.37.0/docs/resources/device_policy_certificates
 // generated from terraform resource schema
 
