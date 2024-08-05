c311c54b78fdd20b3b75defbb15ca81e557204fa
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-05 11:00:28 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/tunnel-config/index.ts
+++ src/tunnel-config/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.38.0/docs/resources/tunnel_config
 // generated from terraform resource schema
 
