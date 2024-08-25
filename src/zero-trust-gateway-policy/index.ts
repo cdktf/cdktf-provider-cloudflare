fd63cabc873f3bba94bccd40cc83c6e6e3db4dd0
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-25 05:51:13 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/zero-trust-gateway-policy/index.ts
+++ src/zero-trust-gateway-policy/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.40.0/docs/resources/zero_trust_gateway_policy
 // generated from terraform resource schema
 
