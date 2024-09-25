86d0eab410eb6124284c1d197a4754e3e85bcd02
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-25 10:04:16 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@3c86295

--- src/tiered-cache/index.ts
+++ src/tiered-cache/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.42.0/docs/resources/tiered_cache
 // generated from terraform resource schema
 
