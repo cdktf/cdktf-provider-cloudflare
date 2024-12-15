e238687b0139f75bfc28184033e38d7c36354152
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-15 05:53:18 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/email-routing-catch-all/index.ts
+++ src/email-routing-catch-all/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.48.0/docs/resources/email_routing_catch_all
 // generated from terraform resource schema
 
