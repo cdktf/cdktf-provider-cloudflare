413a22281fab1bb6ee1e7c0c0c21abc1b8fb3c1d
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-12 05:51:35 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/device-policy-certificates/index.ts
+++ src/device-policy-certificates/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/cloudflare/cloudflare/4.50.0/docs/resources/device_policy_certificates
 // generated from terraform resource schema
 
