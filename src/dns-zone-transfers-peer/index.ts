3bdb141908f104310db773ce4ba028b268dddf2e
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:44:52 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/dns-zone-transfers-peer/index.ts
+++ src/dns-zone-transfers-peer/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/cloudflare/cloudflare/5.5.0/docs/resources/dns_zone_transfers_peer
 // generated from terraform resource schema
 
