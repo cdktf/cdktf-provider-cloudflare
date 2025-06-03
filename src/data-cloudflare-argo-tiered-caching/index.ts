c0ec142da8d5e12d594723ed7784376c44ec6300
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:41:04 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4d35a18

--- src/data-cloudflare-argo-tiered-caching/index.ts
+++ src/data-cloudflare-argo-tiered-caching/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/cloudflare/cloudflare/5.5.0/docs/data-sources/argo_tiered_caching
 // generated from terraform resource schema
 
